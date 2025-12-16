503182c31842c0ca36b4f2b6f21578eae25c9b9f
Tet <LGVP2@Oulook.com>
2025-12-16 09:58:55 +0800

    Hello

---
+++ composeApp/src/androidMain/kotlin/org/example/umineko/Platform.android.kt
@@ -0,0 +1,7 @@
+package org.example.umineko
+
+import android.os.Build
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+
